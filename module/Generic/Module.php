e90b7c677816c0c1e029d3d7a10af9a2d625c3b8
Matthew Setter <matthew.setter@gmail.com>
2013-03-23 13:24:38 +0100

    Added in the required Module class for the Generic module
    This is the basics required for the module to operate.

---
+++ module/Generic/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @version 	0.0.1
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ */
+
+namespace Generic;
+
+class Module
+{
+
+}
